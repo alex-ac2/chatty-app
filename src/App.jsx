7364c038870561a42f8e7efd2c30db048022b57a
Alex Curci <alex.curci@gmail.com.com>
2019-07-02 13:15:49 +0000

    Initial commit of jsx SRC files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
+
+
+function Nav() {
+  return (
+    <nav className="navbar">
+      < a href="/" className="navbar-brand">Chatty</a>
+    </nav>
+  
+  );
+}
+
+
+
+class Main extends Component {
+  render() {
+    return (
+      <MessageList />
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <Nav />
+        <Main />
+        <ChatBar />
+      </div>
     );
   }
 }
---
+++ src/Nav.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+    render() {
+        return (
+            <a href="/" className="navbar-brand">Chatty</a>
+        );
+    }
+}
+
+
+export default Nav;
