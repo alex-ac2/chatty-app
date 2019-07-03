18363b27f40cf59e59f1a0a70bf03693c1916733
Alex Curci <alex.curci@gmail.com.com>
2019-07-03 20:58:31 +0000

    Create notifcation message object to send to socket, render new notification object in message list

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Message from './Message.jsx';
+import Notification from './Notification.jsx';
 
 class MessageList extends Component {
     render() {
@@ -11,6 +12,8 @@ class MessageList extends Component {
             if (message.type === "incomingMessage") {
                 return <Message username={message.username} content={message.content} 
                 id={message.id} key={message.id} />
+            } else if (message.type === "incomingNotification") {
+                return <Notification content={message.content} key={message.id} />
             }
         });
         
