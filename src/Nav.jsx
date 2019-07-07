c05f7c40873ca28184e2de4777dfca244a17c98d
Alex Curci <alex.curci@gmail.com.com>
2019-07-07 07:37:46 +0000

    add startCount for initial client load

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,43 +1,7 @@
 import React, {Component} from 'react';
 import { gql } from 'apollo-boost';
 import {graphql, Subscription} from 'react-apollo';
-import { WebSocketLink } from 'apollo-link-ws';
-import { SubscriptionClient } from "subscriptions-transport-ws";
-import ApolloClient from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-
-// Subscription 
-import { split } from 'apollo-link';
-import { HttpLink } from 'apollo-link-http';
-import { getMainDefinition } from 'apollo-utilities';
-
-
-const GRAPHQL_ENDPOINT = "ws://localhost:5000/graphql";
-
-const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: 'http://localhost:5000/'
-})
-
-const client = new SubscriptionClient(GRAPHQL_ENDPOINT, {
-    reconnect: true,
-  });
-  
-// const link2 = new WebSocketLink(client);
-
-const apolloClient = new ApolloClient({
-    networkInterface: client,
-    cache,
-    link
-});
-
-// const wsLink = new WebSocketLink({
-//     uri: `ws://localhost:5000/subscriptions`,
-//     options: {
-//       reconnect: true
-//     }
-//   });
+import axios from 'axios';
 
 
 const userCountApollo = gql`
@@ -46,33 +10,19 @@ const userCountApollo = gql`
     }
 `;
 
-
-// apolloClient.subscribe({
-//     query: gql`
-//       subscription {
-//           numOfUser 
-//       }`,
-//     variables: {}
-//   }).subscribe({
-//     next (data) {
-//       // Notify your application with the new arrived data
-//       console.log('subscribe socket happened')
-//       console.log('NEW SOCKET DATA: ', data);
-//     }
-//   });
+let startData = [];
+axios.post('http://localhost:5000/graphql', {
+    "query": `query { numOfUser }`
+  })
+  .then(function (response) {
+    startData.push(response.data.data.numOfUser);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 class Nav extends Component {
-    // displayData = () => {
-    //     const data = this.props.data;
-    //     if (data.loading) {
-            
-    //     } else {
-    //         console.log('GQL PROP: ', data);
-    //     }
-    // }
-
-
-    
+   
     render() {
         //console.log('GQL PROP: ', this.props.data.hello);
 
@@ -86,7 +36,7 @@ class Nav extends Component {
                 <Subscription subscription={userCountApollo}> 
                     {({data}) => {
                         console.log('In-Data: ', data);
-                        return <h3>{!data ? "waiting..." : data.numOfUser}</h3>;
+                        return <h3>{!data ? startData[0] : data.numOfUser}</h3>;
                     }}
                 </Subscription> 
                 user(s) online</h3>
